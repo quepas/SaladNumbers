7124ee493a4940f33b95663e3b9a09a54bb3dfe5
Quepas <oneinchman.dev@gmail.com>
2014-11-13 20:33:20 +0100

    Using long int as ModPow arguments type.

--- lib/include/math_method.h
+++ lib/include/math_method.h
@@ -2,8 +2,8 @@
 
 namespace Salad {
 
-unsigned long ModPowNaive(unsigned base, unsigned exponent, unsigned modulus);
-unsigned long ModPow(unsigned base, unsigned exponent, unsigned modulus);
+unsigned long ModPowNaive(unsigned long base, unsigned long exponent, unsigned long modulus);
+unsigned long ModPow(unsigned long base, unsigned long exponent, unsigned long modulus);
 
 }
 // ~~ Salad::math_method
--- lib/src/math_method.cpp
+++ lib/src/math_method.cpp
@@ -4,12 +4,12 @@
 
 namespace Salad {
 
-unsigned long ModPowNaive(unsigned base, unsigned exponent, unsigned modulus)
+unsigned long ModPowNaive(unsigned long base, unsigned long exponent, unsigned long modulus)
 {
   return static_cast<unsigned long>(pow(base, exponent)) % modulus;
 }
 
-unsigned long ModPow(unsigned base, unsigned exponent, unsigned modulus)
+unsigned long ModPow(unsigned long base, unsigned long exponent, unsigned long modulus)
 {
   unsigned long result = 1;
   for (unsigned idx = 0; idx < exponent; ++idx) {
